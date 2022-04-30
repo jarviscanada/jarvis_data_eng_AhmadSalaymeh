6fa0b2a4bf26cdc857c73a1c70d90ec8aca0277d
Ahmad salaymeh <asalaymeh29@gmail.com>
2022-04-30 17:36:12 -0400

    stack via queue

--- practice/src/main/java/ca/jrvs/practice/codingChallenge/Answers.java
+++ practice/src/main/java/ca/jrvs/practice/codingChallenge/Answers.java
@@ -30,15 +30,15 @@ public class Answers {
     hashMap.put('}', '{');
     hashMap.put(']','[');
 
-    // stack will be used to push opening brackets into the stack and pop when ever we get a closing bracket that has the matching pair
+    // myStack will be used to push opening brackets into the myStack and pop when ever we get a closing bracket that has the matching pair
     Stack<Character> stack = new Stack<>();
 
     // convert the S to an array of characters in order to get each single character in the string
     for (char ch: s.toCharArray()){
-      // check if the stack is not empty,
+      // check if the myStack is not empty,
       // check if the current character is a closing bracket
-      // if it is grab the value from the hashmap (opening bracket) and see if it matches the last item in the stack
-      // if it does pop, else push charcter into the stack
+      // if it is grab the value from the hashmap (opening bracket) and see if it matches the last item in the myStack
+      // if it does pop, else push charcter into the myStack
       if(stack.empty()!=true && hashMap.containsKey(ch) && hashMap.get(ch)==stack.peek()){
         stack.pop();
       }else{
@@ -46,7 +46,7 @@ public class Answers {
       }
     }
 
-    //return true if the stack is empty else false
+    //return true if the myStack is empty else false
     return stack.empty();
   }
 
---
+++ practice/src/main/java/ca/jrvs/practice/main/Main.java
@@ -0,0 +1,15 @@
+package ca.jrvs.practice.main;
+import ca.jrvs.practice.dataStructure.*;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    myStack stack = new myStack();
+
+
+   System.out.println(stack.isEmpty());
+
+  }
+
+}
