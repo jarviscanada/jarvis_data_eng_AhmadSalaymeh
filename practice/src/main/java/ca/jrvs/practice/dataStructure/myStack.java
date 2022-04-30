fad145bcd723e4cee7d0cc828f52dd2f2946d09c
Ahmad salaymeh <asalaymeh29@gmail.com>
2022-04-30 18:43:24 -0400

    queue via stack

--- practice/src/main/java/ca/jrvs/practice/dataStructure/myStack.java
+++ practice/src/main/java/ca/jrvs/practice/dataStructure/myStack.java
@@ -71,7 +71,7 @@ public class myStack {
 
   }
 
-  
+
   @Override
   public String toString() {
     return "myStack{" +
--- practice/src/main/java/ca/jrvs/practice/main/Main.java
+++ practice/src/main/java/ca/jrvs/practice/main/Main.java
@@ -1,4 +1,6 @@
 package ca.jrvs.practice.main;
+import static org.junit.Assert.assertEquals;
+
 import ca.jrvs.practice.dataStructure.*;
 
 public class Main {
@@ -6,9 +8,12 @@ public class Main {
   public static void main(String[] args) {
 
     myStack stack = new myStack();
+    myQueue queue = new myQueue();
 
+    queue.add(1);
+    queue.add(2);
+    queue.add(3);
 
-   System.out.println(stack.isEmpty());
 
   }
 
